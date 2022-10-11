efdac8eedf9b610fd2078ab156f6cb24f4133da7
Shawn Song <shawn.song@jumpcloud.com>
2022-10-11 15:24:28 +0800

    added documentation

--- README.md
+++ README.md
@@ -14,15 +14,58 @@
 
 ## Getting Started
 
-### [Option 1] Using Make.com to secure your JumpCloud device enrollment connect key and API keys. 
+###  Option 1 - Using Make.com to secure your JumpCloud device enrollment connect key and API keys.
 
-**Setup the Make Scenrios.**
+**A. Setup the Make Scenrios**
 
 1. [Import the blueprints](https://www.make.com/en/help/scenarios/scenario-editor#6---more) in my repo. 
-2. For Scenario `jcGetConnKey`:
+2. Setup Scenario `jcGetConnKey`:
    * Create a webhook, copy the link, and click on `advanced setting` to add a data structure:
-   [image](https://user-images.githubusercontent.com/19852184/194973929-eb96f4b3-fe41-41bc-8a45-b5b7a80bf265.png)
-   * Make sure `systemKey`, `newHostname` , `groupName` are added as the items in the new data structure. [image](https://user-images.githubusercontent.com/19852184/194973601-396f1b87-4f2f-4689-940f-a01ceb7637cf.png)
+   ![image](https://user-images.githubusercontent.com/19852184/194973929-eb96f4b3-fe41-41bc-8a45-b5b7a80bf265.png)
+   * Make sure `systemKey`, `newHostname` , `groupName` are added as the items in the new data structure. ![image](https://user-images.githubusercontent.com/19852184/194973601-396f1b87-4f2f-4689-940f-a01ceb7637cf.png)
+   * Move on to `validateUser` web request module, add your JC API key (ideally a [Read-only](https://support.jumpcloud.com/support/s/article/JumpCloud-Roles) one.) ![image](https://user-images.githubusercontent.com/19852184/194979520-abedb5d2-652e-4c87-8410-40659db25a37.png)
+        * **Note** You can find the connect key by going to JumpCloud admin console -> Devices -> add Device -> copy the key. 
+   * Move on to `connKeyData` JSON module, create a data structure consists `conn_key`, `email`, and `user_id`, save it.
+   ![image](https://user-images.githubusercontent.com/19852184/194978079-45d246d6-b6d7-4b65-a279-f974d674b96a.png)
+   * Input your JC connect key as a static value on `connKeyData` module, and fill the `email` and `user_id` by the data processed from the `iterator` module :![image](https://user-images.githubusercontent.com/19852184/195002316-6d24620e-21be-40a1-a5ee-bb77160f5afe.png)
 
-steps with screenshots
-https://medium.com/analytics-vidhya/how-to-add-a-screenshot-in-your-github-readme-file-176afeb8ad86
+
+   * Check the rest of the modules and fix any errors. 
+3. Setup Scenario `jcSystemBindUser`:
+   * WIP
+
+4. Setup Scenario `jcSystemAddGroup`:
+   * WIP
+
+**B. Change the Webhook URLs in kickstart.ps1**
+1. Change the URLs respectively created and copied from section A into:
+```pwsh
+$getConnkey_url = "your own webhook"
+$jcSystemBindUser_url = "your own webhook"
+$jcSystemAddGroup_url = "your own webhook"
+```
+
+**C. Kickstart.ps1 hosting**
+Recommended hosting the `Kickstart.ps1` in a publicly accessible, and compliant to your security rquirements. 
+
+It can be:
+* AWS S3 or,
+* Azure blob or,
+* Github
+
+
+\[Optional\] You can self-host `main.psm1` too by changing the url in `kickstart.ps1`:
+```pwsh
+$moduleUrl = "your hosted main.psm1 url"
+```
+
+**D. Update the kickstart.ps1 url in autounattended.xml**
+Once you decided and attained the public url for `kickstart.ps1`, change the url in `autounattended.xml`:
+
+```xml
+<SynchronousCommand wcm:action="add">
+    <Order>4</Order>
+    <CommandLine>PowerShell.exe -WindowStyle Maximized -ExecutionPolicy RemoteSigned iex (irm "your kickstart.ps1 url") </CommandLine>
+    <Description>wap kickoff</Description>
+</SynchronousCommand>
+```
--- kickOff.ps1
+++ kickOff.ps1
@@ -4,41 +4,40 @@
 
 Set-ExecutionPolicy RemoteSigned -Force
 
-#start kicking off
+# Kicking off
 $logfilename = "provisioning"
 
-#import the module
+# Import the module
 $moduleUrl = "https://raw.githubusercontent.com/sonykey2003/win-auto-pilot/master/main.psm1"
 $script = [scriptblock]::Create((Invoke-RestMethod -Method get -Uri $moduleUrl))
 New-Module -Name wap.main -ScriptBlock $script
 
-#set timezone based on the geo location
+# Set timezone based on the geo location
 $countryInfo = Get-GeoApi
 set-tz -countryname $countryInfo.country_name
 
-## https://support.jumpcloud.com/support/s/article/naming-convention-for-users1#convention
-
-#change the hostname to new and not rebooting
+# Change the hostname to new and not rebooting
 $newHostname = Set-Hostname
 
-#installing JC agent
-$getConnkey_url = "https://hook.us1.make.com/b6rlunkty6aff82mc0sy89u3wpl5xkmh"  
+# Installing JC agent
+## Getting the connet key
+$getConnkey_url = "https://hook.us1.make.com/b6rlunkty6aff82mc0sy89u3wpl5xkmh" #change it to your webhook url
 $onboardInfo = getConnKey -url $getConnkey_url 
 $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($onboardInfo.conn_key)
 $conn_Key = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 $jcConfig = "C:\Program Files\JumpCloud\Plugins\Contrib\jcagent.conf" 
  
-
+## installation
 installJCAgent -conn_key $conn_Key 
 $systemKey = (ConvertFrom-Json (Get-Content $jcConfig)).systemKey
 [string]$sudo = "false" #user will not be the local admin on the device by default. 
 [string]$sudoWithoutPW = "false" #sudo with pw option only valid when sudo = true
 
-$jcSystemBindUser_url = "https://hook.us1.make.com/y85m9lxkhgrjlcco6ahzakasg3bt7cr7"
-$jcSystemAddGroup_url = "https://hook.us1.make.com/8hakbgiqck9iojz5mn5e35em6p84q1ry"
+# JC user & system operations
+$jcSystemBindUser_url = "https://hook.us1.make.com/y85m9lxkhgrjlcco6ahzakasg3bt7cr7" #change it to your webhook url
+$jcSystemAddGroup_url = "https://hook.us1.make.com/8hakbgiqck9iojz5mn5e35em6p84q1ry" #change it to your webhook url
 
-#do user binding
-
+## Do user binding
 jcSystemBindUser -url $jcSystemBindUser_url `
  -systemKey $systemKey `
  -newHostname $newHostname `
@@ -46,12 +45,30 @@ jcSystemBindUser -url $jcSystemBindUser_url `
  -sudo $sudo -sudoWithoutPW $sudoWithoutPW
 sleep 10
 
-#add the device to a default group
+## Add the device to a default group
 $groupName = "All Windows"
 jcSysAddGroup -url $jcSystemAddGroup_url -systemKey $systemKey -groupName $groupName
 
-Write-Host "Done!" -ForegroundColor Green
-sleep 5
+# Prep for reboot
+$Banner = @"
+ 
+
+________  ___       ___               ________  ________  ________   _______   ___       
+|\   __  \|\  \     |\  \             |\   ___ \|\   __  \|\   ___  \|\  ___ \ |\  \      
+\ \  \|\  \ \  \    \ \  \            \ \  \_|\ \ \  \|\  \ \  \\ \  \ \   __/|\ \  \     
+ \ \   __  \ \  \    \ \  \            \ \  \ \\ \ \  \\\  \ \  \\ \  \ \  \_|/_\ \  \    
+  \ \  \ \  \ \  \____\ \  \____        \ \  \_\\ \ \  \\\  \ \  \\ \  \ \  \_|\ \ \__\   
+   \ \__\ \__\ \_______\ \_______\       \ \_______\ \_______\ \__\\ \__\ \_______\|__|   
+    \|__|\|__|\|_______|\|_______|        \|_______|\|_______|\|__| \|__|\|_______|   ___ 
+                                                                                     |\__\
+                                                                                     \|__|
+                                                                                          
+                                                                       
+
+"@
+Write-Host $Banner -ForegroundColor Green
+
+sleep 10
 
 Restart-Computer -force
 
